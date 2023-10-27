06c4ad1fe39b93460ff8be56f6014edafd291146
deividgp <dayd4@outlook.com>
2023-10-27 15:09:01 +0200

    Update project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 export default defineConfig({
   site: 'https://deividgp.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind()],
+  server: { port: 1234, host: true}
 });
